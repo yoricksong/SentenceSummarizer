564dcdc54f6271a68f1689800726153a2e9a4cfc
yoricksong <163488142+yoricksong@users.noreply.github.com>
2023-04-05 10:42:01 +0000

    Add docstrings.

---
+++ src/Examples/Program.cs
@@ -0,0 +1,12 @@
+
+﻿using System;
+using System.Collections.Generic;
+
+using Summarizer.Core;
+using Summarizer.Core.Summarizers;
+using Summarizer.Core.KeywordExtractors;
+
+using Summarizer.Infrastructure.Dictionary;
+
+namespace Examples
+{
