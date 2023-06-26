99ca38dfd5b73fdddce6009c9a6b0928a2d23915
yoricksong <163488142+yoricksong@users.noreply.github.com>
2023-06-26 19:12:58 +0000

    Time

--- src/ExtractionProvider.cs
+++ src/ExtractionProvider.cs
@@ -3,4 +3,10 @@ using System.Linq;
 
 namespace Summarizer.Core
 {
-    public abstract class Ex
+    public abstract class ExtractionProvider
+    {
+        /*
+         * Applies the abstract intensity to the amount of keywords by %.
+         * Values 0.0 - 1
+         */
+        public vi
