ebfbba2f3d00b169245abb779d6d74b3fc228664
yoricksong <163488142+yoricksong@users.noreply.github.com>
2023-07-05 19:23:08 +0000

    3.3.0

--- src/IKeywordExtractor.cs
+++ src/IKeywordExtractor.cs
@@ -7,4 +7,9 @@ namespace Summarizer.Core
         /// <summary>
         /// Invoke with the base statement.
         /// </summary>
-      
+        /// <param name="statement"></param>
+        /// <returns>vectorized keywords</returns>
+        Dictionary<string, double> Invoke(string statement);
+
+        /// <summary>
+        /// In
