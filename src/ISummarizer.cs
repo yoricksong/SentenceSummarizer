6b2b8c70465766aa748b3cb9530736d0c2020dd3
yoricksong <163488142+yoricksong@users.noreply.github.com>
2023-07-06 19:01:22 +0000

    small typo fixes

--- src/ISummarizer.cs
+++ src/ISummarizer.cs
@@ -2,4 +2,9 @@
 
 namespace Summarizer.Core
 {
-    public interface ISummarization
+    public interface ISummarizationLayer
+    {
+        Dictionary<string, double> Invoke(string statement);
+        Dictionary<string, double> Invoke(Dictionary<string, double> scoredStatements);
+    }
+}
