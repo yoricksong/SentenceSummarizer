8a9f73d5965ec9c752e1e126238923d6c0d0a8db
yoricksong <163488142+yoricksong@users.noreply.github.com>
2023-07-07 10:20:42 +0000

    remove extra slash

--- src/KeywordExtractors/NlpFrequencyExtractor.cs
+++ src/KeywordExtractors/NlpFrequencyExtractor.cs
@@ -1,2 +1,10 @@
 ﻿using System.Linq;
-using System.Collections.
+using System.Collections.Generic;
+
+using Summarizer.Infrastructure.Nlp;
+
+namespace Summarizer.Core.KeywordExtractors
+{
+    public class NlpFrequencyExtractor : ExtractionProvider, IKeywordExtractor
+    {
+        
