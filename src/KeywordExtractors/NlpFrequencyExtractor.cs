3c788fdb82290da61f3199fade3d111da894eb14
yoricksong <163488142+yoricksong@users.noreply.github.com>
2023-07-07 13:08:17 +0000

    added example files

--- src/KeywordExtractors/NlpFrequencyExtractor.cs
+++ src/KeywordExtractors/NlpFrequencyExtractor.cs
@@ -7,4 +7,10 @@ namespace Summarizer.Core.KeywordExtractors
 {
     public class NlpFrequencyExtractor : ExtractionProvider, IKeywordExtractor
     {
-        
+        public readonly IEnumerable<PosType> OmittedTokens;
+
+        public NlpFrequencyExtractor() : base()
+        {
+            _setupOmmitedTokens(ref OmittedTokens);
+        }
+     
