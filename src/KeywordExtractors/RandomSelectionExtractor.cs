2edc85ec92e2e9c95cc49dcf6f68de9a50a0b9e4
yoricksong <163488142+yoricksong@users.noreply.github.com>
2023-07-25 06:36:30 +0000

    Update pawn.json

--- src/KeywordExtractors/RandomSelectionExtractor.cs
+++ src/KeywordExtractors/RandomSelectionExtractor.cs
@@ -6,4 +6,10 @@ namespace Summarizer.Core.KeywordExtractors
     public class RandomSelectionExtractor : IKeywordExtractor
     {
         public readonly uint MaxKeywords;
-        public readonl
+        public readonly double SelectionLikelihood; // value between 0.0 - 1;
+
+        private readonly Random _random = new Random();
+
+        public RandomSelectionExtractor()
+        {
+            MaxK
