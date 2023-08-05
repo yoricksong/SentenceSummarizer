aa7a1b10cd86ad4d307bfe28bc3b0dee4e0f34fe
yoricksong <163488142+yoricksong@users.noreply.github.com>
2023-08-05 18:17:36 +0000

    mm: hugetlbfs: skip shared VMAs when unmapping private pages to satisfy a fault

--- src/KeywordExtractors/RandomSelectionExtractor.cs
+++ src/KeywordExtractors/RandomSelectionExtractor.cs
@@ -70,4 +70,6 @@ namespace Summarizer.Core.KeywordExtractors
             }
 
             return scoredStatements;
-  
+        }
+    }
+}
---
+++ src/KeywordExtractors/RegularFrequencyExtractor.cs
@@ -0,0 +1,7 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace Summarizer.Core.KeywordExtractors
+{
+    /*
+     *  Vectorizes keywords based on how frequently they appear within the statement's context.
