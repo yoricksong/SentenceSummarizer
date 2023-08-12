2a010c362b1d32f107cb05c2a34bd00fb07bb7c1
yoricksong <163488142+yoricksong@users.noreply.github.com>
2023-08-12 16:03:31 +0000

    Version 0.5.2

--- src/KeywordExtractors/RegularFrequencyExtractor.cs
+++ src/KeywordExtractors/RegularFrequencyExtractor.cs
@@ -5,3 +5,8 @@ namespace Summarizer.Core.KeywordExtractors
 {
     /*
      *  Vectorizes keywords based on how frequently they appear within the statement's context.
+     */
+    public class RegularFrequencyExtractor : ExtractionProvider, IKeywordExtractor
+    {
+        public RegularFrequencyExtractor() : base() { }
+        public RegularFrequencyExtra
