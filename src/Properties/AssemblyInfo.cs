6adcf6eec5d6eac61591988c90eab3aa772953eb
yoricksong <163488142+yoricksong@users.noreply.github.com>
2023-08-18 15:33:00 +0000

    compress image

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -4,3 +4,8 @@ using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SummarizationEngine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(""
