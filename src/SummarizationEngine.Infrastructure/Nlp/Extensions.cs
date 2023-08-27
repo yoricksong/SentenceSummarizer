8ce35e3b4cb1e2f4dd42d1f141c0e3d01f6b1091
yoricksong <163488142+yoricksong@users.noreply.github.com>
2023-08-27 15:52:58 +0000

    flake8 fix

--- src/SummarizationEngine.Infrastructure/Nlp/Extensions.cs
+++ src/SummarizationEngine.Infrastructure/Nlp/Extensions.cs
@@ -51,3 +51,141 @@ namespace Summarizer.Infrastructure.Nlp
                 case PosType.NNP:
                     return "NNP";
                 case PosType.NNPS:
+                    return "NNPS";
+                case PosType.NNS:
+                    return "NNS";
+                case PosType.PDT:
+                    return "PDT";
+                case PosType.POS:
+                    return "POS";
+                case PosType.PRP:
+                    return "PRP";
+                case PosType.PRPS:
+                    return "PRP$";
+                case PosType.RB:
+                    return "RB";
+                case PosType.RBR:
+                    return "RBR";
+                case PosType.RBS:
+                    return "RBS";
+                case PosType.RP:
+                    return "RP";
+                case PosType.SYM:
+                    return "SYM";
+                case PosType.TO:
+                    return "TO";
+                case PosType.UH:
+                    return "UH";
+                case PosType.VB:
+                    return "VB";
+                case PosType.VBD:
+                    return "VBD";
+                case PosType.VBG:
+                    return "VBG";
+                case PosType.VBN:
+                    return "VBN";
+                case PosType.VBP:
+                    return "VBP";
+                case PosType.VBZ:
+                    return "VBZ";
+                case PosType.WDT:
+                    return "WDT";
+                case PosType.WP:
+                    return "WP";
+                case PosType.WPS:
+                    return "WP$";
+                case PosType.WRB:
+                    return "WRB";
+                case PosType.WTF:
+                    return ",";
+                case PosType.WTF_TWO:
+                    return ";";
+            }
+            return null;
+        }        
+        public static PosType ToPos(this string pos)
+        {
+            switch (pos)
+            {
+                case "CC":
+                    return PosType.CC;
+                case "CD":
+                    return PosType.CD;
+                case "DT":
+                    return PosType.DT;
+                case "EX":
+                    return PosType.EX;
+                case "FW":
+                    return PosType.FW;
+                case "IN":
+                    return PosType.IN;
+                case "JJ":
+                    return PosType.JJ;
+                case "JJR":
+                    return PosType.JJR;
+                case "JJS":
+                    return PosType.JJS;
+                case "LS":
+                    return PosType.LS;
+                case "MD":
+                    return PosType.MD;
+                case "NN":
+                    return PosType.NN;
+                case "NNP":
+                    return PosType.NNP;
+                case "NNPS":
+                    return PosType.NNPS;
+                case "NNS":
+                    return PosType.NNS;
+                case "PDT":
+                    return PosType.PDT;
+                case "POS":
+                    return PosType.POS;
+                case "PRP":
+                    return PosType.PRP;
+                case "PRP$":
+                    return PosType.PRPS;
+                case "RB":
+                    return PosType.RB;
+                case "RBR":
+                    return PosType.RBR;
+                case "RBS":
+                    return PosType.RBS;
+                case "RP":
+                    return PosType.RP;
+                case "SYM":
+                    return PosType.SYM;
+                case "TO":
+                    return PosType.TO;
+                case "UH":
+                    return PosType.UH;
+                case "VB":
+                    return PosType.VB;
+                case "VBD":
+                    return PosType.VBD;
+                case "VBG":
+                    return PosType.VBG;
+                case "VBN":
+                    return PosType.VBN;
+                case "VBP":
+                    return PosType.VBP;
+                case "VBZ":
+                    return PosType.VBZ;
+                case "WDT":
+                    return PosType.WDT;
+                case "WP":
+                    return PosType.WP;
+                case "WP$":
+                    return PosType.WPS;
+                case "WRB":
+                    return PosType.WRB;
+                case ",":
+                    return PosType.WTF;
+                case ";":
+                    return PosType.WTF_TWO;
+            }
+
+            return PosType.WTF_TWO;
+        }
+    }    
+}
---
+++ src/SummarizationEngine.Infrastructure/Nlp/POSType.cs
@@ -0,0 +1,2 @@
+﻿
+namespace Summarizer.In
