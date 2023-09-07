04cfc19b8280b5589b7ecc0898d7009db2be0d4c
yoricksong <163488142+yoricksong@users.noreply.github.com>
2023-09-07 11:16:22 +0000

    Package.json updated

--- src/SummarizationEngine.Infrastructure/Properties/AssemblyInfo.cs
+++ src/SummarizationEngine.Infrastructure/Properties/AssemblyInfo.cs
@@ -3,4 +3,8 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
-// set of attributes. Cha
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SummarizationEngine.Infrastructure")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfigura
