84755eb9df4c2016a0631346f765cb723261d0a0
yoricksong <163488142+yoricksong@users.noreply.github.com>
2023-09-12 10:38:13 +0000

    "Initial commit"

--- src/SummarizationEngine.Infrastructure/Properties/AssemblyInfo.cs
+++ src/SummarizationEngine.Infrastructure/Properties/AssemblyInfo.cs
@@ -15,4 +15,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type 
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if t
