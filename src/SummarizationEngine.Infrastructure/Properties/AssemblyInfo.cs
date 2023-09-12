d10af82ca687ef58717ce784d4572d1a389cfa08
yoricksong <163488142+yoricksong@users.noreply.github.com>
2023-09-12 11:36:59 +0000

    various bug fixes

--- src/SummarizationEngine.Infrastructure/Properties/AssemblyInfo.cs
+++ src/SummarizationEngine.Infrastructure/Properties/AssemblyInfo.cs
@@ -19,4 +19,9 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if t
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4414cc05-48d5-476f-888e-b2f2f0e94d58")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major 
