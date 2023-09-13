745207c59e73756fc10c641ba432d906d1f1c170
yoricksong <163488142+yoricksong@users.noreply.github.com>
2023-09-13 06:03:31 +0000

    update sites

--- src/SummarizationEngine.Infrastructure/Properties/AssemblyInfo.cs
+++ src/SummarizationEngine.Infrastructure/Properties/AssemblyInfo.cs
@@ -24,4 +24,11 @@ using System.Runtime.InteropServices;
 
 // Version information for an assembly consists of the following four values:
 //
-//      Major 
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+/
