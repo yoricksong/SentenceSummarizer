8d7f77c0321f006f64cd1ce35c2a18e4fd2b55e0
yoricksong <163488142+yoricksong@users.noreply.github.com>
2023-11-06 08:08:08 +0000

    final tweaks

--- src/SummarizatonHandler.cs
+++ src/SummarizatonHandler.cs
@@ -6,4 +6,7 @@ namespace Summarizer.Core
     public class SummarizatonHandler
     {
         public readonly List<ISummarizationLayer> SummarizerInstances;
-        public readon
+        public readonly bool ShouldFavorLowScores = false;
+        public readonly int MaxStatementLength;
+
+        public SummarizatonHandler(List<ISummarizationLayer> summarizationLayers, int maxStatementLeng
