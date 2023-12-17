8ec0af230393fbf5b663cacf14d88583d268f46e
yoricksong <163488142+yoricksong@users.noreply.github.com>
2023-12-17 11:36:01 +0000

    mailto

---
+++ src/Summarizers/TextRankByFrequency.cs
@@ -0,0 +1,15 @@
+
+﻿using Summarizer.Core.KeywordExtractors;
+
+namespace Summarizer.Core.Summarizers
+{
+    public class TextRankByFrequency : TextRankProvider, ISummarizationLayer
+    {
+        public TextRankByFrequency(IKeywordExtractor extractor) : base(extractor) { } 
+
+        public TextRankByFrequency()
+        {
+            KeywordExtractor = new RegularFrequencyExtractor();
+        }
+    }
+}
